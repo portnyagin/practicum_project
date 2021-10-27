9eeef39cb1e2feeed2ebcd035e7e8518375594ac
portnyagin <shikakito@gmail.com>
2021-10-27 20:36:31 +0300

    account query

--- internal/app/database/query/account.go
+++ internal/app/database/query/account.go
@@ -1,1 +1,16 @@
 package query
+
+const CreateAccount = "INSERT INTO practicum_ut.accounts (id, user_id) VALUES(nextval('seq_account'), $1);"
+
+const updateAccountForUser = "UPDATE practicum_ut.accounts \n" +
+	"SET balance=$2, debit=$3, credit=$4 \n" +
+	"WHERE  user_id=$1;"
+
+const getAccountBalanceForUser = "select balance, debit from accounts where user_id = $1"
+
+/*
+UPDATE practicum_ut.accounts
+SET balance=0, debit=0, credit=0
+WHERE  user_id=0;
+
+select balance, debit from accounts where user_id = 0*/
