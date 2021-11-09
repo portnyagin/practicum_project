4d0e87701f11d8ec8d9189c2668dc50723cdba1c
portnyagin <shikakito@gmail.com>
2021-11-09 21:55:52 +0300

    fix query

--- internal/app/repository/operation.go
+++ internal/app/repository/operation.go
@@ -1,6 +1,6 @@
 package repository
 
-const CreateOperation = "INSERT INTO practicum_ut.operations \n" +
+const CreateOperation = "INSERT INTO operations \n" +
 	"(id, account_id, order_id,order_num, operation_type, amount, processed_at) \n" +
 	"VALUES(nextval('seq_order'), $1, $2, $3, $4, $5, $6);"
 
