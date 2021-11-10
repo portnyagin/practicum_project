e014362d4c805b22fbf1f050a1e2a3cbe9a3ba03
portnyagin <shikakito@gmail.com>
2021-11-10 14:21:31 +0300

    fix order service/ sorting for order list

--- internal/app/repository/order.go
+++ internal/app/repository/order.go
@@ -8,7 +8,7 @@ const UpdateOrderStatus = "UPDATE orders \n" +
 	"SET  status=$2, updated_at=$3 \n" +
 	"where id=$1  and status!=$2;"
 
-const FindOrdersByUser = "select id, num,user_id, status, upload_at, updated_at from orders where user_id = $1;"
+const FindOrdersByUser = "select id, num,user_id, status, upload_at, updated_at from orders where user_id = $1 order by upload_at"
 
 const GetOrderByID = "select id, user_id, num, status, upload_at, updated_at from orders where id = $1;"
 const GetOrderByNum = "select id, user_id, num, status, upload_at, updated_at from orders where num = $1;"
